0e5fcf0955138f33b769beddc63ac7eed3f31d05
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-15 01:11:22 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: HomeActivity(),);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
   }
 }
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key,});
 
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
+
+  @override
+  _HomeActivityState createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
   Future<String> fetchData() async {
     await Future.delayed(Duration(seconds: 2));
     return "Data loaded from FutureBuilder";
@@ -28,93 +36,248 @@ class HomeActivity extends StatelessWidget {
     }
   }
 
+  double boxHeight = 100;
+  double boxWidth = 100;
+  Color boxColor = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Flutter Advance Widget"),centerTitle: true,),
-
-      body: ListView(padding: EdgeInsets.all(10),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Advance Widget"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10),
         children: [
-          //Hero====================
-          Card(child: ListTile(title: Text("Hero Animation"),
-          leading: Hero(tag: "Hero-demo",child: Icon(Icons.star_border, color: Colors.redAccent,),),
-          onTap: (){Navigator.push(context,MaterialPageRoute(builder:(context) => HeroDetailPage()));},),),
+          // Hero====================
+          Card(
+            child: ListTile(
+              title: Text("Hero Animation"),
+              leading: Hero(
+                tag: "Hero-demo",
+                child: Icon(
+                  Icons.star_border,
+                  color: Colors.redAccent,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HeroDetailPage()));
+              },
+            ),
+          ),
+
+          // Dialog=====================
+          Card(
+            child: ListTile(
+              title: Text("Show Dialog"),
+              leading: Icon(
+                Icons.message,
+                color: Colors.green,
+              ),
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => Dialog(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("This Is Dialog"),
+                      ),
+                    ));
+              },
+            ),
+          ),
+
+          // Alert Dialog=========================
+          Card(
+            child: ListTile(
+              title: Text("Alert Dialog"),
+              leading: Icon(
+                Icons.warning_amber,
+                color: Colors.purpleAccent,
+              ),
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text("Are You Sure?"),
+                      content: Text("Are You Want to Sure Exit?"),
+                      actions: [
+                        TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("Cancel")),
+                        TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("Sure"))
+                      ],
+                    ));
+              },
+            ),
+          ),
+
+          // Snack Bar============================
+          Card(
+            child: ListTile(
+              title: Text("This Is Snackbar"),
+              leading: Icon(
+                Icons.notifications,
+                color: Colors.blue,
+              ),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("This Is Snackbar"),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // Bottom Sheet========================
+          Card(
+            child: ListTile(
+              title: Text("Bottom Sheet"),
+              leading: Icon(
+                Icons.arrow_upward,
+                color: Colors.amber,
+              ),
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(
+                      padding: EdgeInsets.all(40),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("This Is Your Bottom Sheet"),
+                          Icon(Icons.email_outlined),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: Text(
+                                "Close",
+                                style: TextStyle(color: Colors.white),
+                              ))
+                        ],
+                      ),
+                    ));
+              },
+            ),
+          ),
+
+          // Future Builder======================
+          Card(
+            child: ListTile(
+              title: Text("Future Builder"),
+              leading: Icon(Icons.hourglass_bottom_outlined,
+                  color: Colors.blue),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text("FutureBuilder Example"),
+                    content: FutureBuilder<String>(
+                      future: fetchData(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return CircularProgressIndicator();
+                        } else if (snapshot.hasError) {
+                          return Text("Error: ${snapshot.error}");
+                        } else {
+                          return Text(snapshot.data ?? "No data");
+                        }
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // Stream Builder===============================
+          Card(
+              child: ListTile(
+                title: Text("Stream Builder"),
+                leading: Icon(
+                  Icons.stream,
+                  color: Colors.teal,
+                ),
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text("Stream Builder Example"),
+                        content: StreamBuilder<int>(
+                            stream: numberStream(),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return Text("Waiting for stream...");
+                              } else if (snapshot.hasError) {
+                                return Text("Error: ${snapshot.error}");
+                              } else {
+                                return Text(
+                                    "Current Value: ${snapshot.data}");
+                              }
+                            }),
+                      ));
+                },
+              )),
+
+          Card(child: ListTile(title: Text("Gesture Detector"),leading: Icon(Icons.gesture),),),
 
 
-        //Dialog=====================
-        Card(child: ListTile(title: Text("Show Dialog"),leading: Icon(Icons.message,color: Colors.green,),
-          onTap: (){showDialog(context: context, builder:(context) =>Dialog(
-            child:Padding(padding: EdgeInsets.all(10),
-              child: Text("This Is Dialog"),),));},
-        ),),
-
-
-          //Alert Dialog=========================
-          Card(child: ListTile(title: Text("Alert Dialog"),leading: Icon(Icons.warning_amber, color: Colors.purpleAccent,),
-          onTap: (){showDialog(context: context, builder:(context) => AlertDialog(
-            title: Text("Are You Sure?"),
-            content: Text("Are You Want to Sure Exit?"),
-            actions: [TextButton(onPressed: () =>Navigator.pop(context), child: Text("Cancel")),
-            TextButton(onPressed: ()=> Navigator.pop(context), child: Text("Sure"))],
-          ));},),),
-
-
-          //Snack Bar============================
-        Card(child: ListTile(title: Text("This Is Snackbar"),
-          leading: Icon(Icons.notifications, color: Colors.blue,),
-          onTap: (){ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text("This Is Snackbar"),duration: Duration(seconds: 1),
-          ),);},
-        ),),
-
-
-          //Bottom Sheet========================
-          Card(child: ListTile(title: Text("Bottom Sheet"),leading: Icon(Icons.arrow_upward, color: Colors.amber,),
-            onTap: (){showModalBottomSheet(context: context, builder:(context) =>
-                Container(padding: EdgeInsets.all(40),
-                  child: Column(mainAxisSize: MainAxisSize.min,
-                  children: [Text("This Is Your Bottom Sheet"), Icon(Icons.email_outlined),
-                    SizedBox(height: 20,),
-                  ElevatedButton(onPressed:()=> Navigator.pop(context), child: Text("Close", style: TextStyle(color: Colors.white),))],),) );},),),
-
-
-          //Future Builder======================
-          Card(child: ListTile(title: Text("Future Builder"),leading: Icon(Icons.hourglass_bottom_outlined, color: Colors.blue),
-          onTap: (){showDialog(context: context, builder: (context)=> AlertDialog(title: Text("FutureBuilder Example"),
-            content: FutureBuilder<String>(
-                future: fetchData(),builder: (context, snapshot){
-                  if (snapshot.connectionState ==  ConnectionState.waiting){
-                    return CircularProgressIndicator();
-                  }
-                  else if(snapshot.hasError){
-                    return Text("Error: ${snapshot.error}");
-                  }
-                  else {
-                    return Text(snapshot.data ?? "No data");
-                  }
-            },),),);},),),
-
-          //Stream Builder===============================
-          Card(child: ListTile(title: Text("Stream Builder"), leading: Icon(Icons.stream, color: Colors.teal,),
-            onTap: (){showDialog(context: context, builder: (context) =>
-                AlertDialog(title: Text("Stream Builder Example"),
-                  content: StreamBuilder<int>(
-                    stream: numberStream(),builder: (context, snapshot){
-                      if (snapshot.connectionState == ConnectionState.waiting){
-                        return Text("Waiting for stream...");
-                      }
-                      else if(snapshot.hasError){
-                        return Text("Error: ${snapshot.error}");
-                      }
-                      else{
-                        return Text("Current Value: ${snapshot.error}");
-                      }
-                  }),));},))
-
-
-        ],),
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+          // Animated Container=============================
+          Card(
+            child: ListTile(
+              title: Text("Animated Container"),
+              leading: Icon(Icons.animation),
+              onTap: () {
+                setState(() {
+                  boxHeight = boxHeight == 50 ? 50 : 50;
+                  boxWidth = boxWidth == 100 ? 200 : 100;
+                  boxColor = boxColor == Colors.blue
+                      ? Colors.green
+                      : Colors.blue;
+                });
+              },
+            ),
+          ),
+
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            height: boxHeight,
+            width: boxWidth,
+            color: boxColor,
+            curve: Curves.easeInOut,
+            alignment: Alignment.center,
+            child: Text("Hey Bro. What's Up?",style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -123,8 +286,17 @@ class HeroDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Hero Detail Page")),
-      body: Center(child: Hero(tag: "Hero-demo",child: Icon(Icons.star,color: Colors.blue,size: 50),),),
+      appBar: AppBar(title: Text("Hero Detail Page")),
+      body: Center(
+        child: Hero(
+          tag: "Hero-demo",
+          child: Icon(
+            Icons.star,
+            color: Colors.blue,
+            size: 50,
+          ),
+        ),
+      ),
     );
-    }
+  }
 }
